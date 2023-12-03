42f088d06f951274383d3460878e69cc58a50db1
vorvit <vorvit@bk.ru>
2023-12-03 03:35:08 +0300

    server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ vue() ],
-  base: '/vuejs-bookstore/',
-  server: { port: '8080' }
+  base: '/vuejs-bookstore/'
 })
